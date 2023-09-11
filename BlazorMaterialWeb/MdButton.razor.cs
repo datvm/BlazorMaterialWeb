9d2d28622f97b256b29e6567ce2aa625aafa3593
Luke Vo <datvm@outlook.com>
2023-09-11 16:29:46 -0500

    Add project files.

---
+++ BlazorMaterialWeb/MdButton.razor.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorMaterialWeb;
+
+partial class MdButton
+{
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public MdButtonType ButtonType { get; set; } = MdButtonType.Elevated;
+
+    string ButtonTagName => ButtonType switch
+    {
+        MdButtonType.Elevated => "md-elevated-button",
+        MdButtonType.Filled => "md-filled-button",
+        MdButtonType.Tonal => "md-tonal-button",
+        MdButtonType.Outlined => "md-outlined-button",
+        MdButtonType.Text => "md-text-button",
+        _ => throw new NotImplementedException(ButtonType.ToString()),
+    };
+
+}
+
+public enum MdButtonType
+{
+    Elevated,
+    Filled,
+    Tonal,
+    Outlined,
+    Text,
+}
