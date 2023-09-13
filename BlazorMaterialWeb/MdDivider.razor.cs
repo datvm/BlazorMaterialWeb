96d2ec2ab0f6dd35713fa01a817adac79b1c05f6
Luke Vo <datvm@outlook.com>
2023-09-12 20:22:43 -0500

    Added Divider

---
+++ BlazorMaterialWeb/MdDivider.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorMaterialWeb;
+
+/// <summary>
+/// Dividers are thin lines that group content in lists or other containers.
+/// <a href="https://m3.material.io/components/divider/overview">Design</a>,
+/// <a href="https://github.com/material-components/material-web/blob/main/docs/components/divider.md">Reference</a>.
+/// </summary>
+partial class MdDivider
+{
+
+    [Parameter]
+    public bool Inset { get; set; }
+
+    [Parameter]
+    public bool InsetStart { get; set; }
+
+    [Parameter]
+    public bool InsetEnd { get; set; }
+
+}
