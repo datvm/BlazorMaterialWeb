1c6ba9586c6ddcc69dc552e7fb93695b3880021f
Luke Vo <datvm@outlook.com>
2023-09-12 20:53:44 -0500

    Added FocusRing

---
+++ BlazorMaterialWeb/MdFocusRing.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorMaterialWeb;
+
+/// <summary>
+/// Focus rings are accessible outlines for components to show keyboard focus.
+/// <a href="https://github.com/material-components/material-web/blob/main/docs/components/focus-ring.md">Documentation</a>
+/// </summary>
+partial class MdFocusRing
+{
+
+    [Parameter]
+    public bool Visible { get; set; }
+
+    [Parameter]
+    public bool Inward { get; set; }
+
+}
