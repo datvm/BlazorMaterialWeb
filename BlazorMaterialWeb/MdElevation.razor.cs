f0eee2f1aae8a35a5e5bc48847b70a8a096c7d7c
Luke Vo <datvm@outlook.com>
2023-09-14 02:15:46 -0500

    Added MdElevation.

---
+++ BlazorMaterialWeb/MdElevation.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorMaterialWeb;
+
+partial class MdElevation
+{
+
+    [Parameter]
+    public string? BorderRadius { get; set; }
+
+    [Parameter, EditorRequired]
+    public int ElevationLevel { get; set; }
+
+    string StyleOutput => string.Join(";",
+        $"--md-elevation-level: {ElevationLevel};",
+        BorderRadius is null ? null : $"--md-elevation-border-radius: {BorderRadius}");
+
+}
