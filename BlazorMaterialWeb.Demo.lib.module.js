5e996c4fab971661cda7e93f02c6fb1e6ed9ef6e
datvm <datvm@outlook.com>
2023-09-20 06:00:09 +0000

    Deploying to gh-pages from @ datvm/BlazorMaterialWeb@a5dcf9061a9c08c444118ca1efff96eaf92f9fc7 🚀

--- BlazorMaterialWeb.Demo.lib.module.js
+++ BlazorMaterialWeb.Demo.lib.module.js
@@ -1,23 +1,51 @@
-export function afterStarted() {
+export function beforeStart() {
 
-    const obs = new MutationObserver((e) => {
-        if (e.some(record => {
-            for (const node of record.addedNodes) {
-                if (node?.querySelector?.("code")) {
-                    return true;
+    function observeCodeHighlight() {
+        const obs = new MutationObserver((e) => {
+            if (e.some(record => {
+                for (const node of record.addedNodes) {
+                    if (node?.querySelector?.("code")) {
+                        return true;
+                    }
+                }
+
+                return false;
+            })) {
+                hljs.highlightAll();
+            }
+        });
+
+        obs.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    }
+
+    function observeMarkdown() {
+        const obs = new MutationObserver((e) => {
+            for (const record of e) {
+                for (const node of record.addedNodes) {
+                    node.querySelectorAll?.("template[data-markdown='true']")?.forEach(el => {
+                        el.setAttribute("data-markdown", "false");
+                        const md = el.textContent.trim();
+
+                        const html = marked.parse(md);
+                        const div = document.createElement("div");
+                        div.innerHTML = html;
+
+                        el.parentElement.insertBefore(div, el);
+                    });
                 }
             }
 
-            return false;
-        })) 
-        {
-            hljs.highlightAll();
-        }
-    });
+        });
 
-    obs.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
+        obs.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    }
 
+    observeCodeHighlight();
+    observeMarkdown();
 }
