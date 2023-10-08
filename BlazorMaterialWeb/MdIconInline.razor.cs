dc40c2c24add8bc936cf6131cea9ddf50258c994
Luke Vo <datvm@outlook.com>
2023-10-07 21:36:49 -0500

    Added MdIcon demo page and added utility MdIconInline (not published yet).

---
+++ BlazorMaterialWeb/MdIconInline.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorMaterialWeb;
+
+partial class MdIconInline
+{
+
+    public const string IconSharp = "material-symbols-sharp";
+    public const string IconOutlined = "material-symbols-outlined";
+    public const string IconRound = "material-symbols-round";
+
+    [Parameter]
+    public string IconClass { get; set; } = IconOutlined;
+
+    [Parameter]
+    public string Class { get; set; } = "";
+
+}
