3ef0ac2ec5579a7b496dfb6ba636465df52059de
Luke Vo <datvm@outlook.com>
2024-02-05 11:11:24 +0700

    Updated the bundled version to v1.2.0

--- pack.ps1
+++ pack.ps1
@@ -4,7 +4,7 @@ foreach($file in $files) {
 	rm $file.FullName
 }
 
-$version="--property:Version=1.1.1"
+$version="--property:Version=1.2.0.0"
 
 dotnet pack .\BlazorMaterialWeb\BlazorMaterialWeb.csproj -c Release "--property:PackageOutputPath=.\bin\nuget" "$version"
 dotnet pack .\BlazorMaterialWeb.Bundled\BlazorMaterialWeb.Bundled.csproj -c Release "--property:PackageOutputPath=.\bin\nuget" "$version"
