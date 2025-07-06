83c65b77dba41385583ab4ecf3adaa0052f42510
Luke Vo <lukevo93@outlook.com>
2025-07-06 02:22:15 -0500

    v1.3.0

---
+++ BlazorMaterialWeb/MdCard.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorMaterialWeb;
+
+partial class MdCard
+{
+
+    [Parameter]
+    public int ElevationLevel { get; set; } = 3;
+
+    [Parameter]
+    public string Class { get; set; } = "";
+
+    [Parameter]
+    public string? ContainerClass { get; set; }
+
+}
